96a65030a360ae236ff356df16585d9173113ed3
GREGALA <goncalo.regalado@mercedes-benz.io>
2021-05-30 18:06:33 +0100

    finished lesson

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://images.e-flux-systems.com/124553_87e1dbc54bb6e4961bd0b950ef52c431.jpg,1440'
         }
     }
 })
